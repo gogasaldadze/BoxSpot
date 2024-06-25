d753ecec6799e753c23fcf51061e1f20fbc343b0
Goga Saldadze <saldadzegoga@gmail.com>
2024-06-25 08:11:59 +0400

    need in lp

--- myproject/products.py
+++ myproject/products.py
@@ -5,7 +5,7 @@ from werkzeug.exceptions import BadRequest
 app = Flask(__name__)
 api = Api(app)
 
-# Custom Validator
+# Custom Validato
 def name_validator(name):
     if not name.isalpha():
         raise BadRequest("Name must contain only alphabetic characters.")
